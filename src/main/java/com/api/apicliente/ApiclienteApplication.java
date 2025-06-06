8127e35548d2703eae1b4b42c94a0321196e5993
Marco Espinoza <marcotk8@gmail.com>
2025-06-05 22:57:50 -0400

    Se terminaron las etapas restantes y sus clases correspondientes.

--- src/main/java/com/api/apicliente/ApiclienteApplication.java
+++ src/main/java/com/api/apicliente/ApiclienteApplication.java
@@ -3,7 +3,7 @@ package com.api.apicliente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.api.apicliente")
 public class ApiclienteApplication {
 
 	public static void main(String[] args) {
